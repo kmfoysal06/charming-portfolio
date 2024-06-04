5d3db29dd4c6165a72de34f4176097fbae1e6659
Kazi Mohammad Foysal <foysalkazimd01@gmail.com>
2024-06-04 15:32:06 +0600

    Initial Commit

---
+++ portfolio.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name: SimpleCharm Portfolio
+ * Description: A simple portfolio plugin for WordPress.
+ * Version: 1.0
+ * Author: Kazi Mohammad Foysal
+ * Author URI: https://profile.wordpress.org/kmfoysal06
+ * /
